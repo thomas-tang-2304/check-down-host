01233aabff19582f93a8f76851b7c202b7691c39
hongtang-digiex <hongtang240@gmail.com>
2023-12-11 15:17:05 +0700

    second commit

--- src/crawl/cheerio/checkCrawlabledLinks.js
+++
@@ -1,5 +0,0 @@
-const checkCrawlabledLinks = (thisLink, originUrl, tag) =>
-  tag == "a" &&
-  thisLink?.startsWith(originUrl) &&
-  !thisLink.includes("#") &&
-  !/\.(png|jpg|webp|avif|jpeg|gif|tiff|svg|pdf)$/i.test(thisLink);
